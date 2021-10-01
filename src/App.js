16b56ce3d6513bc5bf77a2b2948706d323fb09a2
MosamMTL <mosam@moontechnolabs.com>
2021-10-01 17:14:10 +0530

    new one

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ import { AFrameRenderer, Marker } from "react-web-ar"
 function App() {
   return (
     <div className="App">
-      <a-scene embedded arjs='sourceType: webcam;'>
-        <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-        <a-marker-camera preset='hiro'></a-marker-camera>
+      <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;'>
+        <a-marker-camera preset='hiro'>
+          <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+
+        </a-marker-camera>
+        <a-entity camera></a-entity>
       </a-scene>
     </div>
   );
