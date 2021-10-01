52e63e4894e2503f60a5d52723f026dc590c2fe8
MosamMTL <mosam@moontechnolabs.com>
2021-10-01 13:29:07 +0530

    new exp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import {AFrameRenderer,Marker} from "react-web-ar"
+import { AFrameRenderer, Marker } from "react-web-ar"
 
 
 function App() {
   return (
     <div className="App">
       <a-scene embedded arjs='sourceType: webcam;'>
-        {/* <a-marker type='pattern' url='path/to/pattern-marker.patt'>
-          <a-box position='0 0.5 0' material='color: red;'></a-box>
-        </a-marker> */}
-
-        <a-marker preset='Hiro'>
-          <a-box position='0 0.5 0' material='color: green;'></a-box>
-        </a-marker>
-
-        {/* <a-marker type='barcode' value='5'>
-          <a-box position='0 0.5 0' material='color: blue;'></a-box>
-        </a-marker> */}
-
-        <a-entity camera></a-entity>
+        <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+        <a-marker-camera preset='hiro'></a-marker-camera>
       </a-scene>
     </div>
   );
