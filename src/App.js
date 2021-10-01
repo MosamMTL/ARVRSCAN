9e65acb719d4e60b8455ba47aa2ecd9cc6e4f2e5
MosamMTL <mosam@moontechnolabs.com>
2021-10-01 12:23:59 +0530

    add ar vr

--- src/App.js
+++ src/App.js
@@ -4,20 +4,21 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a-scene embedded arjs='sourceType: webcam;'>
+        <a-marker type='pattern' url='path/to/pattern-marker.patt'>
+          <a-box position='0 0.5 0' material='color: red;'></a-box>
+        </a-marker>
+
+        <a-marker preset='hiro'>
+          <a-box position='0 0.5 0' material='color: green;'></a-box>
+        </a-marker>
+
+        <a-marker type='barcode' value='5'>
+          <a-box position='0 0.5 0' material='color: blue;'></a-box>
+        </a-marker>
+
+        <a-entity camera></a-entity>
+      </a-scene>
     </div>
   );
 }
