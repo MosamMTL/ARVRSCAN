f98c3a2a7bf339b882783ebe10daa8102de5e274
MosamMTL <mosam@moontechnolabs.com>
2021-10-01 18:03:00 +0530

    add changes of div and add new code

--- src/App.js
+++ src/App.js
@@ -5,15 +5,10 @@ import { AFrameRenderer, Marker } from "react-web-ar"
 
 function App() {
   return (
-    <div className="App">
-      <a-scene embedded arjs='sourceType: webcam; debugUIEnabled: false;'>
-        <a-marker-camera preset='hiro'>
-          <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
-
-        </a-marker-camera>
-        <a-entity camera></a-entity>
-      </a-scene>
-    </div>
+    <a-scene embedded arjs='sourceType: webcam;'>
+      <a-box position='0 0.5 0' material='opacity: 0.5;'></a-box>
+      <a-marker-camera preset='hiro'></a-marker-camera>
+    </a-scene>
   );
 }
 
