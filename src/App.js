d1affd8a8e2787645ad7c30bbe91a54210a5fefc
MosamMTL <mosam@moontechnolabs.com>
2021-10-01 12:51:46 +0530

    only for hiro

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import {AFrameRenderer,Marker} from "react-web-ar"
+
 
 function App() {
   return (
     <div className="App">
       <a-scene embedded arjs='sourceType: webcam;'>
-        <a-marker type='pattern' url='path/to/pattern-marker.patt'>
+        {/* <a-marker type='pattern' url='path/to/pattern-marker.patt'>
           <a-box position='0 0.5 0' material='color: red;'></a-box>
-        </a-marker>
+        </a-marker> */}
 
         <a-marker preset='hiro'>
           <a-box position='0 0.5 0' material='color: green;'></a-box>
         </a-marker>
 
-        <a-marker type='barcode' value='5'>
+        {/* <a-marker type='barcode' value='5'>
           <a-box position='0 0.5 0' material='color: blue;'></a-box>
-        </a-marker>
+        </a-marker> */}
 
         <a-entity camera></a-entity>
       </a-scene>
